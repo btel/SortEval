eda775c94aebc2317087779b2eff481e29f6e9bb
bartosz <bartosz@aa9929d6-f90f-0410-bdca-858ebc5a397a>
2011-02-07 14:29:00 +0000

    updated sorting scripts
    git-svn-id: https://itb1.biologie.hu-berlin.de/svn/bartosz/Python/PyScripts/SpikeSorting@1638 aa9929d6-f90f-0410-bdca-858ebc5a397a

--- modules/dashboard.py
+++ modules/dashboard.py
@@ -1,21 +1,7 @@
 #!/usr/bin/env python
 #coding=utf-8
-from matplotlib import rcParams
-
-font_size= 10
-params = {'backend': 'WxAgg',
-          'axes.labelsize': font_size,
-          'axes.titlesize': font_size,
-          'text.fontsize': font_size,
-          'xtick.labelsize': font_size,
-          'ytick.labelsize': font_size,
-          'text.usetex': False,
-          'font.size' : font_size,
-          }
-rcParams.update(params)
 
 import numpy as np
-import matplotlib.pyplot as plt
 from scipy import stats
 import tables
 import os, sys
@@ -24,18 +10,57 @@ import hdf5tools
 
 from NeuroTools.parameters import ParameterSet as NTParameterSet
 
-DATAPATH = os.environ['DATAPATH']
+import wx
+import wx.aui
+import matplotlib as mpl
+from matplotlib.backends.backend_wxagg import FigureCanvasWxAgg as Canvas
+from matplotlib.backends.backend_wxagg import NavigationToolbar2Wx as Toolbar
+import matplotlib.pyplot as plt
 
-def plot_psth(dataset, **kwargs):
+class Plot(wx.Panel):
+    def __init__(self, parent, id = -1, dpi = None, **kwargs):
+        wx.Panel.__init__(self, parent, id=id, **kwargs)
+        self.figure = mpl.figure.Figure(dpi=dpi, figsize=(2,2))
+        self.canvas = Canvas(self, -1, self.figure)
+        self.toolbar = Toolbar(self.canvas)
+        self.toolbar.Realize()
+
+        sizer = wx.BoxSizer(wx.VERTICAL)
+        sizer.Add(self.canvas,1,wx.EXPAND)
+        sizer.Add(self.toolbar, 0 , wx.LEFT | wx.EXPAND)
+        self.SetSizer(sizer)
+
+class PlotNotebook(wx.Panel):
+    def __init__(self, parent, id = -1):
+        wx.Panel.__init__(self, parent, id=id)
+        self.nb = wx.aui.AuiNotebook(self)
+        sizer = wx.BoxSizer()
+        sizer.Add(self.nb, 1, wx.EXPAND)
+        self.SetSizer(sizer)
+
+    def add(self,name="plot"):
+       page = Plot(self.nb)
+       self.nb.AddPage(page,name)
+       return page.figure
+
+
+
+
+def plot_psth(ax, dataset, **kwargs):
     spt = dataset['spt']
     stim = dataset['stim']
     ev = dataset['ev']
 
-    patterns.plotPSTH(spt, stim, **kwargs)
+    patterns.plotPSTH(spt, stim,ax=ax, **kwargs)
     ymin, ymax = plt.ylim()
-    plt.vlines(ev, ymin, ymax)
+    if len(ev)>0:
+        plt.vlines(ev, ymin, ymax)
+    
+    ax.text(0.95, 0.9,"total n/o spikes: %d" % (len(spt),),
+            transform=ax.transAxes,
+            ha='right')
 
-def plot_isi(dataset, win=[0,5], bin=0.1):
+def plot_isi(ax, dataset, win=[0,5], bin=0.1, color='k'):
     spt = dataset['spt']
     stim = dataset['stim']
 
@@ -44,80 +69,77 @@ def plot_isi(dataset, win=[0,5], bin=0.1):
     counts, bins = np.histogram(isi, intvs)
     mode,n = stats.mode(isi)
     
-    plt.plot(intvs[:-1], counts, drawstyle='steps-post')
-    plt.axvline(mode, color='k')
+    ax.set_xlim(win)
+    ax.set_ylim(0, counts.max())
+    ax.plot(intvs[:-1], counts, color=color, drawstyle='steps-post')
+    ax.axvline(mode, color='k')
     
-    ax = plt.gca()
-    plt.text(0.95, 0.9,"mode: %.2f ms" % (mode,),
+    ax.text(0.95, 0.9,"mode: %.2f ms" % (mode,),
              transform=ax.transAxes,
             ha='right')
-    plt.xlabel("interval (ms)")
-    plt.ylabel("count")
+    ax.set_xlabel("interval (ms)")
+    ax.set_ylabel("count")
 
-def plot_trains(dataset, **kwargs):
+def plot_trains(ax, dataset, **kwargs):
     spt = dataset['spt']
     stim = dataset['stim']
     ev = dataset['ev']
     
-    patterns.plotraster(spt, stim, **kwargs)
+    patterns.plotraster(spt, stim,ax=ax, **kwargs)
     ymin, ymax = plt.ylim()
-    plt.vlines(ev, ymin, ymax)
+    if len(ev)>0:
+        plt.vlines(ev, ymin, ymax)
 
-def plot_nspikes(dataset, win=[0,30]):
+def plot_nspikes(ax, dataset, win=[0,30], color="k"):
     spt = dataset['spt']
     stim = dataset['stim']
     
     trains = patterns.SortSpikes(spt, stim, win)
     n_spks = np.array([len(t) for t in trains])
     count, bins = np.histogram(n_spks, np.arange(10))
-    plt.bar(bins[:-1]-0.5, count) 
+    ax.bar(bins[:-1]-0.5, count, color=color)
+    ax.set_xlim((-1,10))
 
     burst_frac = np.mean(n_spks>1) 
-    ax = plt.gca()
-    plt.text(0.95, 0.9,"%d %% bursts" % (burst_frac*100,),
+    ax.text(0.95, 0.9,"%d %% bursts" % (burst_frac*100,),
              transform=ax.transAxes,
             ha='right')
-    plt.xlabel("no. spikes")
-    plt.ylabel("count")
+    ax.set_xlabel("no. spikes")
+    ax.set_ylabel("count")
 
 
-def cell_dashboard(h5f, cell):
+def single_cell(stim, spt, fig=None, **kwargs):
 
-    dataset = hdf5tools.read_hdf5_dataset(h5f, cell)
+    if not fig:
+        fig = plt.gcf()
+
+    dataset = {"stim": stim['data'], "spt":spt['data'], "ev":[]}
+
+    #dataset = hdf5tools.read_hdf5_dataset(h5f, cell)
 
     plt.subplots_adjust(hspace=0.3)
 
-    plt.subplot(2,2,1)
-    plot_psth(dataset)
+    ax1=fig.add_subplot(2,2,1)
+    plot_psth(ax1, dataset, **kwargs)
     plt.title("PSTH")
-    plt.subplot(2,2,2)
-    plot_isi(dataset)
+    ax2= fig.add_subplot(2,2,2)
+    plot_isi(ax2, dataset, **kwargs)
     plt.title("ISIH")
-    plt.subplot(2,2,3)
-    plot_trains(dataset)
+    ax3=fig.add_subplot(2,2,3)
+    plot_trains(ax3,dataset)
     plt.title("raster")
-    plt.subplot(2,2,4)
-    plot_nspikes(dataset)
+    ax4= fig.add_subplot(2,2,4)
+    plot_nspikes(ax4, dataset, **kwargs)
     plt.title("burst order")
 
-def main(param_fname, out_dir):
-    
-    params = NTParameterSet(param_fname)
-    h5fname = DATAPATH[:-1]+ params.in_datafile
-    h5f = tables.openFile(h5fname,'r')
-    cells = params.cells
-    
-    summary_dir = os.path.join(out_dir, "summary")
-    os.mkdir(summary_dir)
-    
-    for cell in cells:
-        cell_dashboard(h5f, cell)
-        cell_id = "-".join(cell.split('/')[2:])
-        plt.savefig(os.path.join(summary_dir, cell_id+".png"))
-        plt.close()
+def all_cells(stim, spt_cells, color_func):
+    cell_ids = spt_cells.keys()
 
-if __name__ == "__main__":
-
-    _, param_fname, out_dir = sys.argv
-    main(param_fname, out_dir)
-
+    app = wx.PySimpleApp()
+    frame = wx.Frame(None,-1,'Plotter')
+    plotter = PlotNotebook(frame)
+    for id in cell_ids:
+        fig = plotter.add('cell{0}'.format(id))
+        single_cell(stim, spt_cells[id],fig=fig, color=color_func(id))
+    frame.Show()
+    app.MainLoop()
--- scripts/cluster_bakerlab.py
+++ scripts/cluster_bakerlab.py
@@ -5,13 +5,12 @@
 Based on raw recordings detect spikes, calculate features and do automatic 
 clustering with k-means.
 
-TODO:
-After clustering the spike times are exported back to HDF5 (cell_kmeansX, where 
-X is cluster index)
 """
 
-import numpy as np
 import os, sys
+sys.path.append("../modules")
+import dashboard 
+import numpy as np
 
 import spike_sort as sort
 import spike_sort.ui.manual_sort
@@ -23,18 +22,24 @@ from spike_sort.io import export
 
 DATAPATH = "../data" 
 
+
 if __name__ == "__main__":
 
     dataset = "/Gollum/s39gollum01/el1"
+    cell_templ = "/Gollum/s39gollum01/el1/cell{cell_id}"
+    stim_node = "/".join(dataset.split("/")[:3]+["stim"])
+
     sp_win = [-0.2, 0.8]
-    
+
     start = time.time()
     io_filter = BakerlabFilter("../data/gollum.inf")
-    sp = io_filter.read_sp(dataset,memmap="tables")
-    spt = sort.extract.detect_spikes(sp,  contact=3,
-                                     thresh='auto')
+    export_filter = PyTablesFilter("../data/exported.h5")
+
+    sp = io_filter.read_sp(dataset,memmap="numpy")
+    spt = sort.extract.detect_spikes(sp,  contact=3, thresh='auto')
     
-    spt = sort.extract.align_spikes(sp, spt, sp_win, type="max", resample=10)
+    spt = sort.extract.align_spikes(sp, spt, sp_win, type="max",
+                                    contact=3, resample=10)
     sp_waves = sort.extract.extract_spikes(sp, spt, sp_win)
     features = sort.features.combine(
             (
@@ -56,30 +61,22 @@ if __name__ == "__main__":
     features_cells = sort.features.split_cells(features, clust_idx)
     spikes_cells = sort.extract.split_cells(sp_waves, clust_idx)
     stim = io_filter.read_spt(dataset)
-    
-    from patterns import show, plotPSTH, figure,legend
-    
+    io_filter.close()
+   
+    from matplotlib.pyplot import figure,show
+
     color_map = spike_sort.ui.plotting.label_color(np.unique(spt_cells.keys()))
-    figure()
-    [plotPSTH(spt_cells[i]['data'], stim['data'], 
-              color=color_map(i),
-              label="cell {0}".format(i)) 
-            for i in spt_cells.keys()]
-    legend()
+    for i in spt_cells.keys():
+        #plotPSTH(spt_cells[i]['data'], stim['data'], 
+        #         color=color_map(i),
+        #         label="cell {0}".format(i)) 
+        figure()
+        dashboard.single_cell(stim, spt_cells[i], color=color_map(i))
+    #figure()
+    #spike_sort.ui.plotting.legend(spt_cells.keys(),
+    #                             color_map(spt_cells.keys()))
     show()
-    #cell_templ = "/Gollum/s39gollum01/el1/cell{cell_id}"
-    #export.export_cells(io_filter, cell_templ, spt)
+    #dashboard.all_cells(stim, spt_cells, color_map)
+    #export.export_cells(export_filter, cell_templ, spt)
     
-    #io_filter.close()
-    #from IPython.Shell import IPShellEmbed 
-
-    #ipshell = IPShellEmbed(['--colors NoColor'])
-
-    #ipshell()
-
-    #TODO: export
-    #sort.io.hdf5.write_spt(clust, h5f, cell_node+"_clust",
-    #                           overwrite=True)
-    #sort.io.hdf5.write_spt(rest, h5f, cell_node+"_rest",
-    #                           overwrite=True)
 
